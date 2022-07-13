01f95801660dbbd9f6d4d22381a2ad3a2fd7aa07
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-13 18:30:31 +0800

    Update UserController.java

--- site/src/main/java/com/nero/hua/controller/UserController.java
+++ site/src/main/java/com/nero/hua/controller/UserController.java
@@ -6,16 +6,16 @@ import com.nero.hua.model.base.BaseResponse;
 import com.nero.hua.model.user.RegisterRequest;
 import com.nero.hua.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
 @RestController
-@Controller("user")
+@RequestMapping("user")
 public class UserController {
 
     @Autowired
