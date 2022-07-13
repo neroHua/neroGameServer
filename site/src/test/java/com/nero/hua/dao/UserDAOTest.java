9da5950b27254ff691349d27ca598013ea343aec
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-13 15:56:49 +0800

    Update UserDAOTest.java

--- site/src/test/java/com/nero/hua/dao/UserDAOTest.java
+++ site/src/test/java/com/nero/hua/dao/UserDAOTest.java
@@ -7,7 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
+
+import java.util.Date;
 
 @ActiveProfiles("dev")
 @SpringBootTest
@@ -16,6 +17,22 @@ public class UserDAOTest {
 
     @Autowired
     private UserDAO userDAO;
+
+    @Test
+    public void testInsertUser() {
+        UserDO userDO = new UserDO();
+
+        userDO.setCreateTime(new Date());
+        userDO.setUpdateTime(new Date());
+        userDO.setCreateUserId(0L);
+        userDO.setUpdateUserId(0L);
+
+
+        userDO.setUserId("testCase001");
+        userDO.setPassword("abc123");
+        userDO.setNickName("nickName");
+        userDAO.insertUser(userDO);
+    }
 
     @Test
     public void testSelectByUserId() {
