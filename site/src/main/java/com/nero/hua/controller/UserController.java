a9279b8a1ec5de2502dd34e09eac7a469c7796da
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-13 15:36:50 +0800

    Update UserController.java

--- site/src/main/java/com/nero/hua/controller/UserController.java
+++ site/src/main/java/com/nero/hua/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.nero.hua.controller;
 
 import com.nero.hua.constant.LoginConstant;
-import com.nero.hua.model.LoginRequest;
+import com.nero.hua.model.user.LoginRequest;
 import com.nero.hua.model.base.BaseResponse;
+import com.nero.hua.model.user.RegisterRequest;
 import com.nero.hua.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,10 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
 
 @RestController
+@Controller("user")
 public class UserController {
 
     @Autowired
     UserService userService;
+
+    @PostMapping(value = "register")
+    public BaseResponse<Boolean> register(@RequestBody @Validated RegisterRequest registerRequest) {
+        Boolean successLogin = userService.register(registerRequest);
+
+        return new BaseResponse<>(successLogin);
+    }
 
     @PostMapping(value = "login")
     public BaseResponse<Boolean> login(@RequestBody @Validated LoginRequest loginRequest, HttpSession httpSession) {
