ab7a62efc84ac402703e0cd9d5b3c983c68c2982
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-13 11:17:10 +0800

    Update and rename LoginServiceImpl.java to UserServiceImpl.java

--- site/src/main/java/com/nero/hua/service/impl/UserServiceImpl.java
+++ site/src/main/java/com/nero/hua/service/impl/UserServiceImpl.java
@@ -5,19 +5,19 @@ import com.nero.hua.dao.UserDAO;
 import com.nero.hua.exception.LoginException;
 import com.nero.hua.enumeration.LoginEnumeration;
 import com.nero.hua.model.LoginRequest;
-import com.nero.hua.service.LoginService;
+import com.nero.hua.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LoginServiceImpl implements LoginService {
+public class UserServiceImpl implements UserService {
 
     @Autowired
     UserDAO userDAO;
 
     @Override
     public Boolean login(LoginRequest loginRequest) {
-        UserDO userDO = userDAO.selectById(loginRequest.getUserId());
+        UserDO userDO = userDAO.selectByUserId(loginRequest.getUserId());
 
         if (null == userDO) {
             throw new LoginException(LoginEnumeration.USER_NOT_FOUND);
