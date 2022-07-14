0584c2b32d36150d39c50383c80971d31584ba07
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-14 11:40:48 +0800

    Update BusinessException.java

--- site/src/main/java/com/nero/hua/exception/BusinessException.java
+++ site/src/main/java/com/nero/hua/exception/BusinessException.java
@@ -4,13 +4,14 @@ public class BusinessException extends RuntimeException {
 
     private String code;
 
-    public BusinessException(String message, String code) {
+    public BusinessException(String code, String message) {
         super(message);
         this.code = code;
     }
 
-    public BusinessException(String message, String code, Throwable cause) {
+    public BusinessException(String code, String message, Throwable cause) {
         super(message, cause);
+        this.code = code;
     }
 
     public String getCode() {
