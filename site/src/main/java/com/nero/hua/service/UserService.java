3b60f31428ae31af2fce1ebf281533676e3396ef
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-15 16:33:52 +0800

    Update UserService.java

--- site/src/main/java/com/nero/hua/service/UserService.java
+++ site/src/main/java/com/nero/hua/service/UserService.java
@@ -2,6 +2,7 @@ package com.nero.hua.service;
 
 import com.nero.hua.model.user.LoginRequest;
 import com.nero.hua.model.user.RegisterRequest;
+import com.nero.hua.model.user.UserInformationResponse;
 
 public interface UserService {
 
@@ -9,4 +10,5 @@ public interface UserService {
 
     Boolean login(LoginRequest loginRequest);
 
+    UserInformationResponse getUserInformation(String userId);
 }
