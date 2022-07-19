b08f57eea9fa10fa31d1c9621e448d27bcbf2989
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-19 13:51:02 +0800

    Create GameUserMO.java

---
+++ site/src/main/java/com/nero/hua/model/user/GameUserMO.java
@@ -0,0 +1,18 @@
+package com.nero.hua.model.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GameUserMO {
+
+    private String userId;
+
+    private String userString;
+
+    private String cardList;
+
+    private Long score;
+
+}
