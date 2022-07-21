1f5664d93ad71208a755b7d19de129e9af2f8bea
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-21 13:46:37 +0800

    Update LeaveRoomRequest.java

--- site/src/main/java/com/nero/hua/model/room/LeaveRoomRequest.java
+++ site/src/main/java/com/nero/hua/model/room/LeaveRoomRequest.java
@@ -3,10 +3,15 @@ package com.nero.hua.model.room;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class LeaveRoomRequest {
 
+    @Min(value = 1, message = "房间号不能小于0")
+    @NotNull(message = "房间号不能为空")
     Long roomId;
 
 }
