0814345ae45d9c5d212a53c5ae00830cfc541120
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-21 13:48:17 +0800

    Update RoomService.java

--- site/src/main/java/com/nero/hua/service/RoomService.java
+++ site/src/main/java/com/nero/hua/service/RoomService.java
@@ -2,6 +2,7 @@ package com.nero.hua.service;
 
 import com.nero.hua.model.room.JoinRoomRequest;
 import com.nero.hua.model.room.LeaveRoomRequest;
+import com.nero.hua.model.user.ChangeUserPrepareStatusRequest;
 
 public interface RoomService {
 
@@ -10,4 +11,6 @@ public interface RoomService {
     Boolean joinRoom(String userId, JoinRoomRequest joinRoomRequest);
 
     Boolean leaveRoom(String userId, LeaveRoomRequest leaveRoomRequest);
+
+    Boolean changeUserPrepareStatus(String userId, ChangeUserPrepareStatusRequest changeUserPrepareStatusRequest);
 }
