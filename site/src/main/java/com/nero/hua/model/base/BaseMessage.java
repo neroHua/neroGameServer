91f1986fac3a0d65c660088d7d496a8ca8f19d32
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-22 14:26:23 +0800

    Update BaseMessage.java

--- site/src/main/java/com/nero/hua/model/base/BaseMessage.java
+++ site/src/main/java/com/nero/hua/model/base/BaseMessage.java
@@ -1,7 +1,11 @@
 package com.nero.hua.model.base;
 
 import com.nero.hua.enumeration.MessageTypeEnumeration;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class BaseMessage {
 
     private MessageTypeEnumeration messageTypeEnumeration;
