78bf3991e3bbcbdfd7364a09fb4f97d473c77c22
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-25 16:27:56 +0800

    Update MessageTypeEnumeration.java

--- site/src/main/java/com/nero/hua/enumeration/MessageTypeEnumeration.java
+++ site/src/main/java/com/nero/hua/enumeration/MessageTypeEnumeration.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 @Getter
 public enum MessageTypeEnumeration {
 
+    USER_JOIN_ROOM("userJoinRoom", "用户加入房间"),
+    USER_LEAVE_ROOM("userLeaveRoom", "用户离开房间"),
     CHANGE_USER_PREPARE_STATUS("changeUserPrepareStatus", "用户改变准备状态"),
     DEAL_CARD("dealCard", "发牌");
 
