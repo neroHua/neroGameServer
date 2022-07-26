a89fd8a66f7451912ca81d59f887a0b41e89004b
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-26 17:29:13 +0800

    Create GameManagerTest.java

---
+++ site/src/test/java/com/nero/hua/game/manager/GameManagerTest.java
@@ -0,0 +1,42 @@
+package com.nero.hua.game.manager;
+
+import com.nero.hua.enumeration.CardEnumeration;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GameManagerTest {
+
+    @Test
+    public void testSortCardCase01() {
+        List<CardEnumeration> cardList = new LinkedList<>();
+        cardList.add(CardEnumeration.CARD_103);
+        cardList.add(CardEnumeration.CARD_104);
+
+        GameManager gameManager = new GameManager();
+
+        gameManager.sortOneCardList(0, cardList.size() - 1, cardList);
+
+        Assert.assertEquals(CardEnumeration.CARD_104, cardList.get(0));
+        Assert.assertEquals(CardEnumeration.CARD_103, cardList.get(1));
+    }
+
+    public void testSortCardCase02() {
+        List<CardEnumeration> cardList = new LinkedList<>();
+        cardList.add(CardEnumeration.CARD_103);
+        cardList.add(CardEnumeration.CARD_104);
+        cardList.add(CardEnumeration.CARD_105);
+        cardList.add(CardEnumeration.CARD_106);
+
+        GameManager gameManager = new GameManager();
+
+        gameManager.sortOneCardList(0, cardList.size() - 1, cardList);
+
+        Assert.assertEquals(CardEnumeration.CARD_106, cardList.get(0));
+        Assert.assertEquals(CardEnumeration.CARD_105, cardList.get(1));
+        Assert.assertEquals(CardEnumeration.CARD_104, cardList.get(3));
+        Assert.assertEquals(CardEnumeration.CARD_103, cardList.get(4));
+    }
+}
