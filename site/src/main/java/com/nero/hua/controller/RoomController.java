413fa9a5afdb0950b58c38b9a45ba0501d59b380
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-01 15:47:04 +0800

    Update RoomController.java

--- site/src/main/java/com/nero/hua/controller/RoomController.java
+++ site/src/main/java/com/nero/hua/controller/RoomController.java
@@ -3,6 +3,7 @@ package com.nero.hua.controller;
 import com.nero.hua.model.base.BaseResponse;
 import com.nero.hua.model.room.JoinRoomRequest;
 import com.nero.hua.model.user.ChangeUserPrepareStatusRequest;
+import com.nero.hua.model.user.UserDoPlayCardRequest;
 import com.nero.hua.model.user.RoomUserInformationResponse;
 import com.nero.hua.service.RoomService;
 import com.nero.hua.util.LoginUtil;
@@ -81,6 +82,24 @@ public class RoomController {
         Boolean notRobSuccess = roomService.doNotRobLandlord(userId);
 
         return new BaseResponse<>(notRobSuccess);
+    }
+
+    @PostMapping("doPlayCard")
+    public BaseResponse<Boolean> doPlayCard(@RequestBody @Validated UserDoPlayCardRequest userDoPlayCardRequest, HttpServletRequest httpServletRequest) {
+        String userId = LoginUtil.parseUserIdFromHttpServletRequest(httpServletRequest);
+
+        Boolean playCardSuccess = roomService.doPlayCard(userId, userDoPlayCardRequest);
+
+        return new BaseResponse<>(playCardSuccess);
+    }
+
+    @PostMapping("doNotPlayCard")
+    public BaseResponse<Boolean> doNotPlayCard(HttpServletRequest httpServletRequest) {
+        String userId = LoginUtil.parseUserIdFromHttpServletRequest(httpServletRequest);
+
+        Boolean playCardSuccess = roomService.doNotPlayCard(userId);
+
+        return new BaseResponse<>(playCardSuccess);
     }
 
 }
