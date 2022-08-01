dac2ab28fb3ea0b06864202b94388315d78cde51
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-01 14:09:02 +0800

    Update UserPlayCardTurnMO.java

--- site/src/main/java/com/nero/hua/model/user/UserPlayCardTurnMO.java
+++ site/src/main/java/com/nero/hua/model/user/UserPlayCardTurnMO.java
@@ -2,18 +2,23 @@ package com.nero.hua.model.user;
 
 import com.nero.hua.enumeration.CardEnumeration;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 public class UserPlayCardTurnMO {
 
-    @Getter
     private final int userIndex;
+
+    private final String userId;
 
     private List<CardEnumeration> cardList;
 
-    public UserPlayCardTurnMO(int userIndex) {
+    public UserPlayCardTurnMO(int userIndex, String userId) {
         this.userIndex = userIndex;
+        this.userId = userId;
     }
 
     public boolean userDoNotPlayCard() {
