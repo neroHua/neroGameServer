695604d0108bd67ad8a6e0a49a46503c34c043ec
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-01 10:35:01 +0800

    Update RobLandlordEnumeration.java

--- site/src/main/java/com/nero/hua/enumeration/RobLandlordEnumeration.java
+++ site/src/main/java/com/nero/hua/enumeration/RobLandlordEnumeration.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum RobLandlordEnumeration {
 
+    NOT_TIME_TO_ROB("notTimeToRob", "不是抢地主的时候"),
     NOT_YOUR_TURN_TO_ROB("notYourTurnToRob", "不是您抢地主的轮次"),
     NOT_YOUR_TURN_TO_NOT_ROB("notYourTurnToNotRob", "不是您不抢地主的轮次");
 
