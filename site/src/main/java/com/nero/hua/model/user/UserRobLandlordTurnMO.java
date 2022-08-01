bdf5dd290cd4baad02b98fc05eb9aa822b496c65
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-01 10:37:03 +0800

    Update UserRobLandlordTurnMO.java

--- site/src/main/java/com/nero/hua/model/user/UserRobLandlordTurnMO.java
+++ site/src/main/java/com/nero/hua/model/user/UserRobLandlordTurnMO.java
@@ -8,10 +8,17 @@ import lombok.Setter;
 public class UserRobLandlordTurnMO {
 
     private final int userIndex;
+    private final String userId;
     private boolean doRob;
 
-    public UserRobLandlordTurnMO(int userIndex) {
+    public UserRobLandlordTurnMO(int userIndex, String userId) {
         this.userIndex = userIndex;
+        this.userId = userId;
     }
 
+    public UserRobLandlordTurnMO(int userIndex, String userId, boolean doRob) {
+        this.userIndex = userIndex;
+        this.userId = userId;
+        this.doRob = doRob;
+    }
 }
