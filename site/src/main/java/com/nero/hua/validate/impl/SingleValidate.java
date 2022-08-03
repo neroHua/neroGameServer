5d0596be22a6f13b471bba7bb59e4540bd286f7a
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-03 16:51:17 +0800

    Create SingleValidate.java

---
+++ site/src/main/java/com/nero/hua/validate/impl/SingleValidate.java
@@ -0,0 +1,21 @@
+package com.nero.hua.validate.impl;
+
+import com.nero.hua.enumeration.CardEnumeration;
+import com.nero.hua.enumeration.PlayCardTypeEnumeration;
+import com.nero.hua.validate.PlayCardTypeValidate;
+
+import java.util.List;
+
+public class SingleValidate implements PlayCardTypeValidate {
+
+    private static final int COUNT = 1;
+    @Override
+    public PlayCardTypeEnumeration getPlayCardTypeEnumeration() {
+        return PlayCardTypeEnumeration.SINGLE;
+    }
+
+    @Override
+    public boolean match(List<CardEnumeration> cardEnumerationList) {
+        return COUNT == cardEnumerationList.size() ? Boolean.TRUE : Boolean.FALSE;
+    }
+}
