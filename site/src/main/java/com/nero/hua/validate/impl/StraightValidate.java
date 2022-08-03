dca52c50781a05dad265a6844d21693c2421fb59
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-03 16:51:30 +0800

    Create StraightValidate.java

---
+++ site/src/main/java/com/nero/hua/validate/impl/StraightValidate.java
@@ -0,0 +1,37 @@
+package com.nero.hua.validate.impl;
+
+import com.nero.hua.enumeration.CardEnumeration;
+import com.nero.hua.enumeration.PlayCardTypeEnumeration;
+import com.nero.hua.validate.PlayCardTypeValidate;
+
+import java.util.List;
+
+public class StraightValidate implements PlayCardTypeValidate {
+
+    private static final int MIN_COUNT = 5;
+
+    @Override
+    public PlayCardTypeEnumeration getPlayCardTypeEnumeration() {
+        return PlayCardTypeEnumeration.STRAIGHT;
+    }
+
+    @Override
+    public boolean match(List<CardEnumeration> cardEnumerationList) {
+        if (cardEnumerationList.size() < MIN_COUNT) {
+            return Boolean.FALSE;
+        }
+
+        for (int i = 1; i < cardEnumerationList.size(); i++) {
+           if (cardEnumerationList.get(i).getValue() - 1 != cardEnumerationList.get(i - 1).getValue()) {
+               return Boolean.FALSE;
+           }
+        }
+
+        if (cardEnumerationList.get(cardEnumerationList.size() - 1).getValue() >= CardEnumeration.CARD_415.getValue()) {
+            return Boolean.FALSE;
+        }
+
+        return Boolean.TRUE;
+    }
+
+}
