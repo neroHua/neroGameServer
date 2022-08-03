de8bb9d1729d8a1b0c84228b4cc36da0e9926243
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-03 16:52:07 +0800

    Create TripleValidate.java

---
+++ site/src/main/java/com/nero/hua/validate/impl/TripleValidate.java
@@ -0,0 +1,28 @@
+package com.nero.hua.validate.impl;
+
+import com.nero.hua.enumeration.CardEnumeration;
+import com.nero.hua.enumeration.PlayCardTypeEnumeration;
+import com.nero.hua.validate.PlayCardTypeValidate;
+
+import java.util.List;
+
+public class TripleValidate implements PlayCardTypeValidate {
+
+    private static final int COUNT = 3;
+
+    @Override
+    public PlayCardTypeEnumeration getPlayCardTypeEnumeration() {
+        return PlayCardTypeEnumeration.TRIPLE;
+    }
+
+    @Override
+    public boolean match(List<CardEnumeration> cardEnumerationList) {
+        if (COUNT != cardEnumerationList.size()) {
+            return Boolean.FALSE;
+        }
+
+        return cardEnumerationList.get(0).getValue() == cardEnumerationList.get(1).getValue()
+            && cardEnumerationList.get(1).getValue() == cardEnumerationList.get(2).getValue();
+    }
+
+}
