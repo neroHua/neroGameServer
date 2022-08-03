ca40422be57873db0f5d8c1b1c1abbc000b829ad
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-03 16:46:14 +0800

    Create PlayCardTypeEnumeration.java

---
+++ site/src/main/java/com/nero/hua/enumeration/PlayCardTypeEnumeration.java
@@ -0,0 +1,34 @@
+package com.nero.hua.enumeration;
+
+import lombok.Getter;
+
+@Getter
+public enum PlayCardTypeEnumeration {
+
+    SINGLE("single", 0, "单牌"),
+    STRAIGHT("straight", 0, "顺子"),
+
+    PAIR("pair", 0, "对子"),
+    PAIR_STRAIGHT("pairStraight", 0, "连对"),
+
+    TRIPLE("triple", 0, "三不带"),
+    TRIPLE_SINGLE("tripleSingle", 0, "三带一"),
+    TRIPLE_PAIR("triplePAIR", 0, "三带一对"),
+
+    AIRPLANE("airplane", 0, "飞机不带"),
+    AIRPLANE_SINGLE("airplaneSingle", 0, "飞机带单"),
+    AIRPLANE_PAIR("airplanePair", 0, "飞机带对"),
+
+    BOMB("bomb", 1, "炸弹"),
+    BOMB_KING("bombKing", 2, "王炸");
+
+    private String code;
+    private int value;
+    private String message;
+
+    PlayCardTypeEnumeration(String code, int value, String message) {
+        this.code = code;
+        this.value = value;
+        this.message = message;
+    }
+}
