337fc500e9d46e3414a7230b003efd6c60a2e106
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-03 17:59:46 +0800

    Update UserDoPlayCardMessage.java

--- site/src/main/java/com/nero/hua/model/user/UserDoPlayCardMessage.java
+++ site/src/main/java/com/nero/hua/model/user/UserDoPlayCardMessage.java
@@ -2,6 +2,7 @@ package com.nero.hua.model.user;
 
 import com.nero.hua.enumeration.CardEnumeration;
 import com.nero.hua.enumeration.MessageTypeEnumeration;
+import com.nero.hua.enumeration.PlayCardTypeEnumeration;
 import com.nero.hua.model.base.BaseMessage;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +14,15 @@ import java.util.List;
 public class UserDoPlayCardMessage extends BaseMessage {
 
     private String userId;
+
     private List<CardEnumeration> cardEnumerationList;
 
-    public UserDoPlayCardMessage(String userId, List<CardEnumeration> cardEnumerationList) {
+    private PlayCardTypeEnumeration playCardTypeEnumeration;
+
+    public UserDoPlayCardMessage(String userId, List<CardEnumeration> cardEnumerationList, PlayCardTypeEnumeration playCardTypeEnumeration) {
         super(MessageTypeEnumeration.USER_DO_PLAY_CARD);
         this.userId = userId;
         this.cardEnumerationList = cardEnumerationList;
+        this.playCardTypeEnumeration = playCardTypeEnumeration;
     }
 }
