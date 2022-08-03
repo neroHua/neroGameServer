74a6a2ebd962e84f2958f7a4b70945aca7edfb51
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-03 17:19:28 +0800

    Update CardUtil.java

--- site/src/main/java/com/nero/hua/util/CardUtil.java
+++ site/src/main/java/com/nero/hua/util/CardUtil.java
@@ -1,10 +1,47 @@
 package com.nero.hua.util;
 
 import com.nero.hua.enumeration.CardEnumeration;
+import com.nero.hua.enumeration.PlayCardTypeEnumeration;
+import com.nero.hua.model.user.UserPlayCardTurnMO;
+import com.nero.hua.validate.PlayCardTypeValidate;
+import com.nero.hua.validate.impl.*;
 
 import java.util.*;
 
 public class CardUtil {
+
+    public static Map<PlayCardTypeEnumeration, PlayCardTypeValidate> playCardTypeValidateMap = new HashMap<>();
+
+    static {
+        PlayCardTypeValidate playCardTypeValidate = new SingleValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+        playCardTypeValidate = new StraightValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+
+        playCardTypeValidate = new PairValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+        playCardTypeValidate = new PairStraightValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+
+        playCardTypeValidate = new TripleValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+        playCardTypeValidate = new TripleSingleValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+        playCardTypeValidate = new TriplePairValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+
+        playCardTypeValidate = new AirplaneValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+        playCardTypeValidate = new AirplaneSingleValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+        playCardTypeValidate = new AirplanePairValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+
+        playCardTypeValidate = new BombValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+        playCardTypeValidate = new BombKingValidate();
+        playCardTypeValidateMap.put(playCardTypeValidate.getPlayCardTypeEnumeration(), playCardTypeValidate);
+    }
 
     public static Map<CardEnumeration, Integer> convertCardListToCardMap(List<CardEnumeration> cardList) {
         Map<CardEnumeration, Integer> cardMap = new HashMap<>();
@@ -32,4 +69,43 @@ public class CardUtil {
         }
         return cardList;
     }
+
+    public static boolean playCardNotMatchPlayCardType(List<CardEnumeration> cardEnumerationList, PlayCardTypeEnumeration playCardTypeEnumeration) {
+        return playCardTypeValidateMap.get(playCardTypeEnumeration).match(cardEnumerationList);
+    }
+
+    public static boolean handCardMapContainsPlayCardMap(Map<CardEnumeration, Integer> handCardMap, Map<CardEnumeration, Integer> playCardMap) {
+        return false;
+    }
+
+    public static boolean handCardMapNotContainsPlayCardMap(Map<CardEnumeration, Integer> handCardMap, Map<CardEnumeration, Integer> playCardMap) {
+        return !handCardMapContainsPlayCardMap(handCardMap, playCardMap);
+    }
+
+    public static boolean currentPlayCardListBetterThanLastPlayCardList(UserPlayCardTurnMO lastUserPlayCardTurnMO, List<CardEnumeration> playCardList, PlayCardTypeEnumeration playCardTypeEnumeration) {
+        if (null == lastUserPlayCardTurnMO) {
+            return Boolean.TRUE;
+        }
+
+        PlayCardTypeEnumeration lastPlayCardTypeEnumeration = lastUserPlayCardTurnMO.getPlayCardTypeEnumeration();
+        if (playCardTypeEnumeration.getValue() < lastPlayCardTypeEnumeration.getValue()) {
+            return Boolean.FALSE;
+        }
+        else if (playCardTypeEnumeration.getValue() > lastPlayCardTypeEnumeration.getValue()) {
+            return Boolean.TRUE;
+        }
+
+        if (lastPlayCardTypeEnumeration != playCardTypeEnumeration) {
+            return Boolean.FALSE;
+        }
+
+        List<CardEnumeration> lastPlayCardList = lastUserPlayCardTurnMO.getCardList();
+        return lastPlayCardList.size() == playCardList.size()
+                && lastPlayCardList.get(0).getValue() >= playCardList.get(0).getValue();
+    }
+
+    public static boolean currentPlayCardListNotBetterThanLastPlayCardList(UserPlayCardTurnMO lastUserPlayCardTurnMO, List<CardEnumeration> playCardList, PlayCardTypeEnumeration playCardTypeEnumeration) {
+        return !currentPlayCardListBetterThanLastPlayCardList(lastUserPlayCardTurnMO, playCardList, playCardTypeEnumeration);
+    }
+
 }
