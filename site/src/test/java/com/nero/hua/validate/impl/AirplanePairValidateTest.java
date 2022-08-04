aca8b36fbbd178205378e5b8828b121bb4ffe76c
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-04 13:50:04 +0800

    Create AirplanePairValidateTest.java

---
+++ site/src/test/java/com/nero/hua/validate/impl/AirplanePairValidateTest.java
@@ -0,0 +1,70 @@
+package com.nero.hua.validate.impl;
+
+import com.nero.hua.enumeration.CardEnumeration;
+import com.nero.hua.validate.PlayCardTypeValidate;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AirplanePairValidateTest {
+
+    @Test
+    public void testCase01() {
+        PlayCardTypeValidate playCardTypeValidate = new AirplanePairValidate();
+
+        List<CardEnumeration> cardEnumerationList = new LinkedList<>();
+
+        cardEnumerationList.add(CardEnumeration.CARD_113);
+        cardEnumerationList.add(CardEnumeration.CARD_213);
+        cardEnumerationList.add(CardEnumeration.CARD_313);
+        cardEnumerationList.add(CardEnumeration.CARD_114);
+        cardEnumerationList.add(CardEnumeration.CARD_214);
+        cardEnumerationList.add(CardEnumeration.CARD_314);
+
+        cardEnumerationList.add(CardEnumeration.CARD_303);
+        cardEnumerationList.add(CardEnumeration.CARD_403);
+        cardEnumerationList.add(CardEnumeration.CARD_304);
+        cardEnumerationList.add(CardEnumeration.CARD_404);
+        Assert.assertTrue(playCardTypeValidate.match(cardEnumerationList));
+
+
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+
+        cardEnumerationList.add(CardEnumeration.CARD_115);
+        cardEnumerationList.add(CardEnumeration.CARD_215);
+        cardEnumerationList.add(CardEnumeration.CARD_315);
+
+        cardEnumerationList.add(CardEnumeration.CARD_303);
+        cardEnumerationList.add(CardEnumeration.CARD_403);
+        cardEnumerationList.add(CardEnumeration.CARD_304);
+        cardEnumerationList.add(CardEnumeration.CARD_404);
+
+        cardEnumerationList.add(CardEnumeration.CARD_305);
+        cardEnumerationList.add(CardEnumeration.CARD_405);
+        Assert.assertFalse(playCardTypeValidate.match(cardEnumerationList));
+
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+        cardEnumerationList.remove(cardEnumerationList.size() - 1);
+
+        cardEnumerationList.add(CardEnumeration.CARD_303);
+        cardEnumerationList.add(CardEnumeration.CARD_403);
+        cardEnumerationList.add(CardEnumeration.CARD_304);
+        cardEnumerationList.add(CardEnumeration.CARD_404);
+
+        cardEnumerationList.remove(4);
+        Assert.assertFalse(playCardTypeValidate.match(cardEnumerationList));
+    }
+}
