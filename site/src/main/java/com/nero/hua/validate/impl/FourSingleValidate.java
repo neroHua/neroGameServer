82ca9b2c5af845603872883428b3d7f6ec2d0721
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-04 14:44:08 +0800

    Create FourSingleValidate.java

---
+++ site/src/main/java/com/nero/hua/validate/impl/FourSingleValidate.java
@@ -0,0 +1,29 @@
+package com.nero.hua.validate.impl;
+
+import com.nero.hua.enumeration.CardEnumeration;
+import com.nero.hua.enumeration.PlayCardTypeEnumeration;
+import com.nero.hua.validate.PlayCardTypeValidate;
+
+import java.util.List;
+
+public class FourSingleValidate implements PlayCardTypeValidate {
+
+    private static final int COUNT = 6;
+
+    @Override
+    public PlayCardTypeEnumeration getPlayCardTypeEnumeration() {
+        return PlayCardTypeEnumeration.FOUR_SINGLE;
+    }
+
+    @Override
+    public boolean match(List<CardEnumeration> cardEnumerationList) {
+        if (COUNT != cardEnumerationList.size()) {
+            return Boolean.FALSE;
+        }
+
+        return cardEnumerationList.get(0).getValue() == cardEnumerationList.get(1).getValue()
+            && cardEnumerationList.get(1).getValue() == cardEnumerationList.get(2).getValue()
+            && cardEnumerationList.get(2).getValue() == cardEnumerationList.get(3).getValue();
+    }
+
+}
