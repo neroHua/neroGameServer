abf5f80f44dcc9289b5b2893757a7149f0024dad
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-04 16:47:20 +0800

    Create CardUtilTest.java

---
+++ site/src/test/java/com/nero/hua/util/CardUtilTest.java
@@ -0,0 +1,40 @@
+package com.nero.hua.util;
+
+import com.nero.hua.enumeration.CardEnumeration;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CardUtilTest {
+
+    @Test
+    public void testSortCardCase01() {
+        List<CardEnumeration> cardList = new LinkedList<>();
+        cardList.add(CardEnumeration.CARD_103);
+        cardList.add(CardEnumeration.CARD_104);
+
+        CardUtil.sortOneCardList(0, cardList.size() - 1, cardList);
+
+        Assert.assertEquals(CardEnumeration.CARD_104, cardList.get(0));
+        Assert.assertEquals(CardEnumeration.CARD_103, cardList.get(1));
+    }
+
+    @Test
+    public void testSortCardCase02() {
+        List<CardEnumeration> cardList = new LinkedList<>();
+        cardList.add(CardEnumeration.CARD_103);
+        cardList.add(CardEnumeration.CARD_104);
+        cardList.add(CardEnumeration.CARD_105);
+        cardList.add(CardEnumeration.CARD_106);
+
+        CardUtil.sortOneCardList(0, cardList.size() - 1, cardList);
+
+        Assert.assertEquals(CardEnumeration.CARD_106, cardList.get(0));
+        Assert.assertEquals(CardEnumeration.CARD_105, cardList.get(1));
+        Assert.assertEquals(CardEnumeration.CARD_104, cardList.get(2));
+        Assert.assertEquals(CardEnumeration.CARD_103, cardList.get(3));
+    }
+
+}
