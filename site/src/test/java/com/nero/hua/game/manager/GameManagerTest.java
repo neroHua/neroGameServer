3a1d1ffa5f08bbebbdf5e148498069943afa8a5f
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-04 16:45:42 +0800

    Update GameManagerTest.java

--- site/src/test/java/com/nero/hua/game/manager/GameManagerTest.java
+++ site/src/test/java/com/nero/hua/game/manager/GameManagerTest.java
@@ -1,6 +1,5 @@
 package com.nero.hua.game.manager;
 
-import com.nero.hua.enumeration.CardEnumeration;
 import com.nero.hua.model.user.GameUserMO;
 import org.junit.Assert;
 import org.junit.Test;
@@ -70,38 +69,6 @@ public class GameManagerTest {
     }
 
     @Test
-    public void testSortCardCase01() {
-        List<CardEnumeration> cardList = new LinkedList<>();
-        cardList.add(CardEnumeration.CARD_103);
-        cardList.add(CardEnumeration.CARD_104);
-
-        GameManager gameManager = new GameManager();
-
-        gameManager.sortOneCardList(0, cardList.size() - 1, cardList);
-
-        Assert.assertEquals(CardEnumeration.CARD_104, cardList.get(0));
-        Assert.assertEquals(CardEnumeration.CARD_103, cardList.get(1));
-    }
-
-    @Test
-    public void testSortCardCase02() {
-        List<CardEnumeration> cardList = new LinkedList<>();
-        cardList.add(CardEnumeration.CARD_103);
-        cardList.add(CardEnumeration.CARD_104);
-        cardList.add(CardEnumeration.CARD_105);
-        cardList.add(CardEnumeration.CARD_106);
-
-        GameManager gameManager = new GameManager();
-
-        gameManager.sortOneCardList(0, cardList.size() - 1, cardList);
-
-        Assert.assertEquals(CardEnumeration.CARD_106, cardList.get(0));
-        Assert.assertEquals(CardEnumeration.CARD_105, cardList.get(1));
-        Assert.assertEquals(CardEnumeration.CARD_104, cardList.get(2));
-        Assert.assertEquals(CardEnumeration.CARD_103, cardList.get(3));
-    }
-
-    @Test
     public void testChooseOneUserToRobLandlord() {
         GameManager gameManager = new GameManager();
 
@@ -155,7 +122,6 @@ public class GameManagerTest {
 
         gameManager.makeLastUserRobLandlordCard(gameUserMOList);
         Assert.assertTrue(gameManager.getRobLandlordRoundMO().getUserRobLandlordTurnMOList().get(2).isDoRob());
-
     }
 
 }
