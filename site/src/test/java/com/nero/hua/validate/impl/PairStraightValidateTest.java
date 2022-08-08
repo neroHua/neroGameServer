2e4d74674f9758264c5e1b497c969edfdc67c9ad
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-08 14:54:23 +0800

    Update PairStraightValidateTest.java

--- site/src/test/java/com/nero/hua/validate/impl/PairStraightValidateTest.java
+++ site/src/test/java/com/nero/hua/validate/impl/PairStraightValidateTest.java
@@ -15,25 +15,46 @@ public class PairStraightValidateTest {
         PlayCardTypeValidate playCardTypeValidate = new PairStraightValidate();
 
         List<CardEnumeration> cardEnumerationList = new LinkedList<>();
-        cardEnumerationList.add(CardEnumeration.CARD_110);
-        cardEnumerationList.add(CardEnumeration.CARD_210);
-        cardEnumerationList.add(CardEnumeration.CARD_111);
-        cardEnumerationList.add(CardEnumeration.CARD_211);
-        cardEnumerationList.add(CardEnumeration.CARD_112);
-        cardEnumerationList.add(CardEnumeration.CARD_212);
-        cardEnumerationList.add(CardEnumeration.CARD_113);
-        cardEnumerationList.add(CardEnumeration.CARD_213);
         cardEnumerationList.add(CardEnumeration.CARD_114);
         cardEnumerationList.add(CardEnumeration.CARD_214);
+        cardEnumerationList.add(CardEnumeration.CARD_113);
+        cardEnumerationList.add(CardEnumeration.CARD_213);
+        cardEnumerationList.add(CardEnumeration.CARD_112);
+        cardEnumerationList.add(CardEnumeration.CARD_212);
         Assert.assertTrue(playCardTypeValidate.match(cardEnumerationList));
+    }
 
+    @Test
+    public void testCase02() {
+        PlayCardTypeValidate playCardTypeValidate = new PairStraightValidate();
+
+        List<CardEnumeration> cardEnumerationList = new LinkedList<>();
         cardEnumerationList.add(CardEnumeration.CARD_115);
         cardEnumerationList.add(CardEnumeration.CARD_215);
+        cardEnumerationList.add(CardEnumeration.CARD_114);
+        cardEnumerationList.add(CardEnumeration.CARD_214);
+        cardEnumerationList.add(CardEnumeration.CARD_113);
+        cardEnumerationList.add(CardEnumeration.CARD_213);
+        cardEnumerationList.add(CardEnumeration.CARD_112);
+        cardEnumerationList.add(CardEnumeration.CARD_212);
         Assert.assertFalse(playCardTypeValidate.match(cardEnumerationList));
 
         cardEnumerationList.remove(cardEnumerationList.size() - 1);
         cardEnumerationList.remove(cardEnumerationList.size() - 1);
         cardEnumerationList.remove(4);
+        Assert.assertFalse(playCardTypeValidate.match(cardEnumerationList));
+    }
+
+    @Test
+    public void testCase03() {
+        PlayCardTypeValidate playCardTypeValidate = new PairStraightValidate();
+
+        List<CardEnumeration> cardEnumerationList = new LinkedList<>();
+        cardEnumerationList.add(CardEnumeration.CARD_114);
+        cardEnumerationList.add(CardEnumeration.CARD_214);
+        cardEnumerationList.add(CardEnumeration.CARD_113);
+        cardEnumerationList.add(CardEnumeration.CARD_112);
+        cardEnumerationList.add(CardEnumeration.CARD_212);
         Assert.assertFalse(playCardTypeValidate.match(cardEnumerationList));
     }
 }
