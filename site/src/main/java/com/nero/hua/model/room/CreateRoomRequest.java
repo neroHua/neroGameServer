f47b49c38da23340d1f53df3ac3625fd2d36b5cf
neroHua <38644203+neroHua@users.noreply.github.com>
2022-09-15 11:12:55 +0800

    Update CreateRoomRequest.java

--- site/src/main/java/com/nero/hua/model/room/CreateRoomRequest.java
+++ site/src/main/java/com/nero/hua/model/room/CreateRoomRequest.java
@@ -4,14 +4,12 @@ import com.nero.hua.enumeration.GameTypeEnumeration;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class CreateRoomRequest {
 
-    @Min(value = 1, message = "游戏类型不能小于0")
     @NotNull(message = "游戏类型不能为空")
     GameTypeEnumeration gameTypeEnumeration;
 
