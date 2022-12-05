d8d7a6a321fda667dc991150ba3aaf5c1fdc9814
neroHua <38644203+neroHua@users.noreply.github.com>
2022-12-05 16:19:30 +0800

    Create PlayCardTip.java

---
+++ site/src/main/java/com/nero/hua/game/tip/PlayCardTip.java
@@ -0,0 +1,145 @@
+package com.nero.hua.game.tip;
+
+import com.nero.hua.enumeration.CardEnumeration;
+import com.nero.hua.enumeration.GameTypeEnumeration;
+import com.nero.hua.enumeration.PlayCardTypeEnumeration;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PlayCardTip {
+
+    public static List<List<Integer>> tip(List<CardEnumeration> formatHandCardEnumerationList, List<CardEnumeration> formatPlayCardEnumerationList, PlayCardTypeEnumeration playCardTypeEnumeration, GameTypeEnumeration gameTypeEnumeration) {
+        if (GameTypeEnumeration.FIGHT_LANDLORD_FOR_THREE == gameTypeEnumeration) {
+            return tipForThree(formatHandCardEnumerationList, formatPlayCardEnumerationList, playCardTypeEnumeration);
+        }
+        return null;
+    }
+
+    private static List<List<Integer>> tipForThree(List<CardEnumeration> formatHandCardEnumerationList, List<CardEnumeration> formatPlayCardEnumerationList, PlayCardTypeEnumeration playCardTypeEnumeration) {
+        if (PlayCardTypeEnumeration.SINGLE == playCardTypeEnumeration) {
+            return findBigSingleInFormatHandCardListForThree(formatHandCardEnumerationList, formatPlayCardEnumerationList);
+        }
+        else if (PlayCardTypeEnumeration.STRAIGHT.getValue() == playCardTypeEnumeration.getValue()) {
+            return findBigStraightInFormatHandCardListForThree(formatHandCardEnumerationList, formatPlayCardEnumerationList);
+        }
+        else if (PlayCardTypeEnumeration.BOMB.getValue() == playCardTypeEnumeration.getValue()) {
+            return findBigBombInFormatHandCardListForThree(formatHandCardEnumerationList, formatPlayCardEnumerationList);
+        }
+        else if (PlayCardTypeEnumeration.BOMB_KING == playCardTypeEnumeration) {
+            return null;
+        }
+
+        return null;
+    }
+
+    private static List<List<Integer>> findBigSingleInFormatHandCardListForThree(List<CardEnumeration> formatHandCardEnumerationList, List<CardEnumeration> formatPlayCardEnumerationList) {
+        List<List<Integer>> bigList = new ArrayList<>();
+        for (int i = 0; i < formatHandCardEnumerationList.size(); i++) {
+            CardEnumeration cardCurrent = formatHandCardEnumerationList.get(i);
+            if (cardCurrent.getValue() > formatPlayCardEnumerationList.get(0).getValue()) {
+                bigList.add(Arrays.asList(i));
+            }
+        }
+        return bigList;
+    }
+
+    private static List<List<Integer>> findBigStraightInFormatHandCardListForThree(List<CardEnumeration> formatHandCardEnumerationList, List<CardEnumeration> formatPlayCardEnumerationList) {
+        if (CardEnumeration.CARD_114.getValue() == formatPlayCardEnumerationList.get(0).getValue()) {
+            return null;
+        }
+
+        final int PLAY_CARD_SIZE = formatPlayCardEnumerationList.size();
+        if (formatHandCardEnumerationList.size() < PLAY_CARD_SIZE) {
+            return null;
+        }
+
+        List<List<Integer>> bigTempList = new ArrayList<>();
+        List<Integer> tempList= new ArrayList<>();
+        for (int i = 0; i < formatHandCardEnumerationList.size() - 1; ) {
+            CardEnumeration cardCurrent = formatHandCardEnumerationList.get(i);
+            if (cardCurrent.getValue() > CardEnumeration.CARD_114.getValue()) {
+                i++;
+                continue;
+            }
+
+            if (cardCurrent.getValue() <= formatPlayCardEnumerationList.get(PLAY_CARD_SIZE - 1).getValue()) {
+                break;
+            }
+
+            CardEnumeration cardEnumerationNext = formatHandCardEnumerationList.get(i + 1);
+            while (cardCurrent.getValue() == cardEnumerationNext.getValue()) {
+                i++;
+                cardCurrent = cardEnumerationNext;
+                cardEnumerationNext = formatHandCardEnumerationList.get(i);
+            }
+            tempList.add(i);
+
+            if (cardCurrent.getValue() - 1 == cardEnumerationNext.getValue()) {
+                if (i == formatHandCardEnumerationList.size() - 2) {
+                    tempList.add(i + 1);
+                    if (tempList.size() >= PLAY_CARD_SIZE) {
+                        bigTempList.add(tempList);
+                    }
+                }
+                i++;
+            }
+            else {
+                if (tempList.size() >= PLAY_CARD_SIZE) {
+                    bigTempList.add(tempList);
+                }
+                else {
+                    tempList = new ArrayList<>();
+                }
+                i++;
+            }
+        }
+
+        if (CollectionUtils.isEmpty(bigTempList)) {
+            return null;
+        }
+
+        List<List<Integer>> bigList = new ArrayList<>();
+        for (List<Integer> list : bigTempList) {
+            for (int i = 0; i <= list.size() - PLAY_CARD_SIZE; i++) {
+                List<Integer> tempBigList = new ArrayList<>();
+                for (int j = i; j < list.size(); j++) {
+                    tempBigList.add(list.get(j));
+                }
+                bigList.add(tempBigList);
+            }
+        }
+
+        return bigList;
+    }
+
+    private static List<List<Integer>> findBigBombInFormatHandCardListForThree(List<CardEnumeration> formatHandCardEnumerationList, List<CardEnumeration> formatPlayCardEnumerationList) {
+        List<List<Integer>> bigList = new ArrayList<>();
+        final int COUNT = 4;
+        int count = 1;
+        for (int i = 0; i < formatHandCardEnumerationList.size() - 1; i++) {
+            CardEnumeration cardCurrent = formatHandCardEnumerationList.get(i);
+            if (cardCurrent.getValue() < formatPlayCardEnumerationList.get(0).getValue()) {
+                break;
+            }
+            CardEnumeration cardEnumerationNext = formatHandCardEnumerationList.get(i);
+            if (cardCurrent.getValue() == cardEnumerationNext.getValue()) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (COUNT == count) {
+                if (cardCurrent.getValue() > formatPlayCardEnumerationList.get(0).getValue()) {
+                    List<Integer> big = Arrays.asList(i - 2, i - 1, i, i + 1);
+                    bigList.add(big);
+                }
+            }
+        }
+
+        return bigList;
+    }
+
+}
