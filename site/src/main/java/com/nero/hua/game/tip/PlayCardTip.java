651d6b246d7cc66dfcb2022255da24e9e01beeba
neroHua <38644203+neroHua@users.noreply.github.com>
2023-01-28 11:36:14 +0800

    Update PlayCardTip.java

--- site/src/main/java/com/nero/hua/game/tip/PlayCardTip.java
+++ site/src/main/java/com/nero/hua/game/tip/PlayCardTip.java
@@ -28,6 +28,9 @@ public class PlayCardTip {
         else if (PlayCardTypeEnumeration.PAIR == playCardTypeEnumeration) {
             return findBigPairInFormatHandCardListForThree(formatHandCardEnumerationList, formatPlayCardEnumerationList);
         }
+        else if (PlayCardTypeEnumeration.PAIR_STRAIGHT == playCardTypeEnumeration) {
+            return findBigPairStraightInFormatHandCardListForThree(formatHandCardEnumerationList, formatPlayCardEnumerationList);
+        }
         else if (PlayCardTypeEnumeration.BOMB.getValue() == playCardTypeEnumeration.getValue()) {
             return findBigBombInFormatHandCardListForThree(formatHandCardEnumerationList, formatPlayCardEnumerationList);
         }
@@ -158,6 +161,91 @@ public class PlayCardTip {
 
         return bigList;
     }
+
+    private static List<List<Integer>> findBigPairStraightInFormatHandCardListForThree(List<CardEnumeration> formatHandCardEnumerationList, List<CardEnumeration> formatPlayCardEnumerationList) {
+        if (CardEnumeration.CARD_114.getValue() == formatPlayCardEnumerationList.get(0).getValue()) {
+            return null;
+        }
+
+        final int PLAY_CARD_SIZE = formatPlayCardEnumerationList.size();
+        if (formatHandCardEnumerationList.size() < PLAY_CARD_SIZE) {
+            return null;
+        }
+
+        final int COUNT = 2;
+        List<List<Integer>> bigTempList = new ArrayList<>();
+        List<Integer> tempList= new ArrayList<>();
+        for (int i = 0; i < formatHandCardEnumerationList.size(); ) {
+            CardEnumeration cardCurrent = formatHandCardEnumerationList.get(i);
+            if (cardCurrent.getValue() > CardEnumeration.CARD_114.getValue()) {
+                i++;
+                continue;
+            }
+
+            if (cardCurrent.getValue() <= formatPlayCardEnumerationList.get(PLAY_CARD_SIZE - 1).getValue()) {
+                break;
+            }
+
+            int tempCount = 1;
+            if (i == formatHandCardEnumerationList.size() - 1) {
+                if (tempCount >= COUNT) {
+                    tempList.add(i);
+                    if (tempList.size() >= PLAY_CARD_SIZE) {
+                        bigTempList.add(tempList);
+                    }
+                    break;
+                }
+            }
+
+            CardEnumeration cardEnumerationNext = formatHandCardEnumerationList.get(i + 1);
+            while (null != cardEnumerationNext && cardCurrent.getValue() == cardEnumerationNext.getValue()) {
+                i++;
+                tempCount++;
+                cardCurrent = cardEnumerationNext;
+                cardEnumerationNext = i + 1 > formatHandCardEnumerationList.size() - 1 ? null : formatHandCardEnumerationList.get(i + 1);
+            }
+            if (tempCount >= COUNT) {
+                tempList.add(i);
+            }
+
+            if (null == cardEnumerationNext) {
+                if (tempList.size() >= PLAY_CARD_SIZE / COUNT) {
+                    bigTempList.add(tempList);
+                }
+                break;
+            }
+
+            if (cardCurrent.getValue() - 1 != cardEnumerationNext.getValue()) {
+                if (tempList.size() >= PLAY_CARD_SIZE / COUNT) {
+                    bigTempList.add(tempList);
+                }
+                tempList = new ArrayList<>();
+            }
+            i++;
+        }
+
+        if (CollectionUtils.isEmpty(bigTempList)) {
+            return null;
+        }
+
+        List<List<Integer>> bigList = new ArrayList<>();
+        for (List<Integer> list : bigTempList) {
+            for (int i = 0; i <= list.size() - PLAY_CARD_SIZE / COUNT; i++) {
+                List<Integer> tempBigList = new ArrayList<>();
+                for (int j = i; j < PLAY_CARD_SIZE / COUNT + i; j++) {
+                    List<Integer> duplicateList = new ArrayList<>();
+                    for (int k = COUNT; k >= 1; k--) {
+                        duplicateList.add(list.get(j) - k + 1);
+                    }
+                    tempBigList.addAll(duplicateList);
+                }
+                bigList.add(tempBigList);
+            }
+        }
+
+        return bigList;
+    }
+
 
     private static List<List<Integer>> findBigBombInFormatHandCardListForThree(List<CardEnumeration> formatHandCardEnumerationList, List<CardEnumeration> formatPlayCardEnumerationList) {
         final int BOMB_DUPLICATE_COUNT = 4;
