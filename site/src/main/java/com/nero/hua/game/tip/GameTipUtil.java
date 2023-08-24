7e19a95ef5d2bcbc3a7f76692497dd816ca86f0a
D-V9042LG7-1201\huadeyou <2676337735@qq.com>
2023-08-24 15:24:05 +0800

    finish GameTipUtil

--- site/src/main/java/com/nero/hua/game/tip/GameTipUtil.java
+++ site/src/main/java/com/nero/hua/game/tip/GameTipUtil.java
@@ -10,13 +10,15 @@ public class GameTipUtil {
 
     public static List<List<Integer>> tip(List<CardEnumeration> formatHandCardEnumerationList, List<CardEnumeration> formatPlayCardEnumerationList, PlayCardTypeEnumeration playCardTypeEnumeration, GameTypeEnumeration gameTypeEnumeration) {
         if (GameTypeEnumeration.FIGHT_LANDLORD_FOR_THREE == gameTypeEnumeration) {
-            return tipForThree(formatHandCardEnumerationList, formatPlayCardEnumerationList, playCardTypeEnumeration);
+            return new GameTipForThree().tip(formatHandCardEnumerationList, formatPlayCardEnumerationList, playCardTypeEnumeration);
+        }
+        else if (GameTypeEnumeration.FIGHT_LANDLORD_FOR_FOUR == gameTypeEnumeration) {
+            return new GameTipForFour().tip(formatHandCardEnumerationList, formatPlayCardEnumerationList, playCardTypeEnumeration);
+        }
+        else if (GameTypeEnumeration.FIGHT_LANDLORD_FOR_FIVE == gameTypeEnumeration) {
+            return new GameTipForFive().tip(formatHandCardEnumerationList, formatPlayCardEnumerationList, playCardTypeEnumeration);
         }
         return null;
-    }
-
-    private static List<List<Integer>> tipForThree(List<CardEnumeration> formatHandCardEnumerationList, List<CardEnumeration> formatPlayCardEnumerationList, PlayCardTypeEnumeration playCardTypeEnumeration) {
-        return new GameTipForThree().tip(formatHandCardEnumerationList, formatPlayCardEnumerationList, playCardTypeEnumeration);
     }
 
 }
